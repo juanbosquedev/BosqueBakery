9f7836e781ee30cd2a0dab09ebeab0eb6a8eeb10
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-07-28 12:04:33 -0300

    Style Home

--- src/home.jsx
+++ src/home.jsx
@@ -1,6 +1,7 @@
+import Style from './home.module.css'
 export default function Home (){
     return(
-        <div>
+        <div className={Style.homeMain}>
             <h1>principal<br/>
             aca puede ir una imagen de fondo y luego se va a mostrar informacion que se seleccione<br/>
             ej: productos y sus fotos</h1>
