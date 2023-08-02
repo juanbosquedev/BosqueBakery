6f3d67c3b0c47bcca7625d149c861b63668fbe02
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-08-02 09:15:32 -0300

    card branch style

--- src/branches/branches.jsx
+++ src/branches/branches.jsx
@@ -1,27 +1,33 @@
 import { Link } from "react-router-dom";
 import branches from "./branchJSON.JS";
+import style from "./branches.module.css";
 
 function Branches() {
-  console.log(branches.map((branche) => branche));
   return (
     <>
-      <div>
+      
         {branches.map((branche) => (
-          <h1 key={branche.name}>
-            {branche.name}
-            <br />
-            {branche.adress}
-            <br />
-            {branche.adressImg && (
-              <Link to={branche.adressLink} target="_blank" >
-                <img src={branche.adressImg} alt={branche.name} />
-              </Link>
-            )}
-          </h1>
+          <div className={style.branche_body} key={branche.name}>
+
+            <main className={style.branche_main}>
+              {branche.name}
+              <br />
+              {branche.adress}
+            </main>
+
+            <div className={style.branche_map}>
+              {branche.adressImg && (
+                <Link to={branche.adressLink} target="_blank">
+                  <img src={branche.adressImg} alt={branche.name} className={style.branche_map_img}/>
+                  <br/>map
+                </Link>
+              )}
+            </div>
+          </div>
         ))}
 
         <Link to="/home">voler</Link>
-      </div>
+     
     </>
   );
 }
