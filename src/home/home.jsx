ad71ce1cfd24e3d61e76089134fc72b1e23b67cd
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-08-01 22:32:01 -0300

    styling

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,9 +1,14 @@
 import Style from './home.module.css'
+
 export default function Home (){
     return(
         <div className={Style.homeMain}>
             <h1>Inicio<br/>
            galería de imagenes 20% vw</h1>
+           <div>
+
+           </div>
         </div>
     )
 }
+
