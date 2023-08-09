e447ba1dbdeaa3ce92d049214729f10c2f59b5b0
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-08-09 09:00:48 -0300

    button top funtionality

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -5,54 +5,30 @@ import Contact from "../contact/contact";
 import About from "../about/about";
 import Products from "../prodcuts/products";
 import style from "./App.module.css";
-import { useState, useEffect } from 'react';
+import ScrollButton from "./Scroll";
+
 
 function App() {
-  
 
-  
- 
-    const [isVisible, setIsVisible] = useState(false);
-  
-    useEffect(() => {
-      const handleScroll = () => {
-        if (window.scrollY > 100) { // Define tu propio valor de umbral
-          setIsVisible(true);
-        } else {
-          setIsVisible(false);
-        }
-      };
-  
-      window.addEventListener('scroll', handleScroll);
-  
-      return () => {
-        window.removeEventListener('scroll', handleScroll);
-      };
-    }, []);
-  
-    const scrollToTop = () => {
-      window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-      });
-    };
+
   return (
+
     <div className={style}>
+
       <Nav />
+
       <section>
-      <button
-      className={`scroll-button ${isVisible ? 'visible' : ''}`}
-      onClick={scrollToTop}
-    >
-      Subir
-    </button>
+      <ScrollButton />
         <Home />
         <About />
         <Branches />
         <Contact />
         <Products />
       </section>
+
+
     </div>
+
   );
 }
 
---
+++ src/app/Scroll.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import style from "./Scroll.module.css";
+
+const ScrollButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        // Define tu propio valor de umbral
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <button
+      id={style.Scroll}
+      className={`scroll-button ${isVisible ? "visible" : ""}`}
+      onClick={scrollToTop}
+    //   style={{ display: isVisible ? 'block' : 'none' }}
+    >
+      ^
+    </button>
+  );
+};
+
+export default ScrollButton;
