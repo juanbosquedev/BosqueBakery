160287fc3bf69b2e5d4e516599bb567ab4b337d9
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-08-08 21:31:02 -0300

    button top

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -5,12 +5,47 @@ import Contact from "../contact/contact";
 import About from "../about/about";
 import Products from "../prodcuts/products";
 import style from "./App.module.css";
+import { useState, useEffect } from 'react';
 
 function App() {
+  
+
+  
+ 
+    const [isVisible, setIsVisible] = useState(false);
+  
+    useEffect(() => {
+      const handleScroll = () => {
+        if (window.scrollY > 100) { // Define tu propio valor de umbral
+          setIsVisible(true);
+        } else {
+          setIsVisible(false);
+        }
+      };
+  
+      window.addEventListener('scroll', handleScroll);
+  
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, []);
+  
+    const scrollToTop = () => {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    };
   return (
     <div className={style}>
       <Nav />
       <section>
+      <button
+      className={`scroll-button ${isVisible ? 'visible' : ''}`}
+      onClick={scrollToTop}
+    >
+      Subir
+    </button>
         <Home />
         <About />
         <Branches />
