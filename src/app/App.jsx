d536f85ac63c8d8584d663cf0861188f4c678f4e
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-08-12 23:54:03 -0300

    lazy load

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -5,30 +5,29 @@ import Contact from "../contact/contact";
 import About from "../about/about";
 import Products from "../prodcuts/products";
 import style from "./App.module.css";
-import ScrollButton from "./Scroll";
+// import ScrollButton from "./Scroll";
+import { lazy, Suspense } from "react"; //importacion dinámica
 
+const ScrollButton = lazy(() => import("./Scroll")); //promesa, se ejecuta la importación una vez que es utilizado en el componente, por eso es dinámico, sino no se iporta hasta tanto
 
 function App() {
-
-
   return (
-
     <div className={style}>
-
       <Nav />
 
       <section>
-      <ScrollButton />
+        <Suspense fallback={<div>rendering</div>}>
+          {/*resolución de la promesa, mensaje de renderizado*/}
+          <ScrollButton />
+        </Suspense>
+
         <Home />
         <About />
         <Branches />
         <Contact />
         <Products />
       </section>
-
-
     </div>
-
   );
 }
 
