92fe797d5df38e1670a38b37476b0d0d5f9bbdb2
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-08-18 10:20:27 -0300

    branche styles

--- src/branches/branches.jsx
+++ src/branches/branches.jsx
@@ -3,13 +3,11 @@ import branches from "./branchJSON.JS";
 import style from "./branches.module.css";
 
 function Branches() {
-  
   return (
     <div id="sucursales">
-      
-        {branches.map((branche) => (
-          <div className={style.branche_body} key={branche.name}>
-
+      {branches.map((branche) => (
+        <div className={style.branche} key={branche.name}>
+          <div className={style.branche_body}>
             <main className={style.branche_main}>
               {branche.name}
               <br />
@@ -18,18 +16,28 @@ function Branches() {
 
             <div className={style.branche_map}>
               {branche.adressImg && (
-                <Link to={branche.adressLink} target="_blank">
-                  <img src={branche.adressImg} alt={branche.name} className={style.branche_map_img}/>
-                  <br/>map
+                <Link
+                  to={branche.adressLink}
+                  target="_blank"
+                  className={style.branche_link}
+                >
+                  <img
+                    src={branche.adressImg}
+                    alt={branche.name}
+                    className={style.branche_map_img}
+                  />
+                  {/* <br /> */}
+                  map
                 </Link>
               )}
             </div>
-            
           </div>
-        ))}
+        </div>
+      ))}
 
-        <a href="#home" className={style.branche_comeBack_button}>voler</a>
-     
+      <a href="#home" className={style.branche_comeBack_button}>
+        voler
+      </a>
     </div>
   );
 }
