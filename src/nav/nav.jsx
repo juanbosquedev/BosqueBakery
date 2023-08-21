20479559663fe60a1c3c974ce7b8482d22bd7efa
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-08-21 09:40:36 -0300

    backgroud nav && styles

--- src/nav/nav.jsx
+++ src/nav/nav.jsx
@@ -1,18 +1,21 @@
-import style from './nav.module.css'
+import style from "./nav.module.css";
+import logo from "../assets/logo.png";
 
 export default function Nav() {
   return (
-    <div className={style.Nav_main} id="Nav">
-      <a href="#inicio">Inicio</a>
-      <br />
-      <a href="#productos">Productos</a>
-      <br />
-      <a href="#nosotros">Nosotros</a>
-      <br />
-      <a href="#sucursales">Sucursales</a>
-      <br />
-      <a href="#contacto">Contacto</a>
-      <br />
+    <div className={style.Nav} id="Nav">
+      <img className={style.Nav_logo} src={logo} alt="logo" />
+      <section className={style.Nav_main}>
+        <a href="#inicio">Inicio</a>
+        <br />
+        <a href="#productos">Productos</a>
+        <br />
+        <a href="#nosotros">Nosotros</a>
+        <br />
+        <a href="#sucursales">Sucursales</a>
+        <br />
+        <a href="#contacto">Contacto</a>
+      </section>
     </div>
   );
 }
