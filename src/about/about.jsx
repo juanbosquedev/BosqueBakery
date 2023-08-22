876cab0868f4d0a70cdf29a2cffd83f020f2414e
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-08-22 20:54:04 -0300

    branch styles

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,35 +1,38 @@
+import style from './about.module.css'
 export default function About() {
   return (
     <div id="nosotros">
-      <h6>
-        Nosotros: &quot;Hecho con Amor&quot; Somos la esencia de tres
-        generaciones que han tejido su corazón y alma en cada pan que hemos
-        horneado. Refleja la pasión que dedicamos a cada producto y la forma en
-        que conectamos con las personas a través de nuestras creaciones,
-        alimentando no solo el cuerpo, sino también los sueños.
-        <br /> <br />
-        historia
-        <br /> <br />
-        Misión: En Panadería Bosque, tejemos el amor y la tradición en cada
-        rincón de nuestro pan. Desde 1967, hemos amasado sueños y sabores en
-        cada hogar, conectando generaciones a través de la magia de la panadería
-        artesanal. Nuestra misión es nutrir corazones y cuerpos con productos
-        horneados con pasión, generando momentos que perduran en el tiempo y
-        alimentan el alma de las familias.
-        <br /> <br />
-        Valores: La esencia de Bosque reside en el amor y la dedicación de
-        nuestra familia, que ha cuidado cada receta y cada proceso con cariño
-        durante tres generaciones. Honramos el legado de nuestro viejo horno a
-        leña, fusionando la autenticidad de antaño con la innovación de hoy para
-        ofrecer experiencias únicas y sabores inolvidables. No comprometemos en
-        lo que respecta al sabor y la frescura. Más que una panadería, somos un
-        punto de encuentro donde las personas se reúnen para compartir, celebrar
-        y crear recuerdos. Valoramos nuestra conexión con la comunidad y la
-        enriquecemos con cada pan que horneamos. Buscamos inspirar no solo con
-        nuestros productos, sino también con la pasión que ponemos en todo lo
-        que hacemos. Nuestra creatividad y dedicación se reflejan en cada
-        detalle.
-      </h6>
+      <div className={style.about_main}>
+        <h6>
+          Nosotros: &quot;Hecho con Amor&quot; Somos la esencia de tres
+          generaciones que han tejido su corazón y alma en cada pan que hemos
+          horneado. Refleja la pasión que dedicamos a cada producto y la forma
+          en que conectamos con las personas a través de nuestras creaciones,
+          alimentando no solo el cuerpo, sino también los sueños.
+          <br /> <br />
+          historia
+          <br /> <br />
+          Misión: En Panadería Bosque, tejemos el amor y la tradición en cada
+          rincón de nuestro pan. Desde 1967, hemos amasado sueños y sabores en
+          cada hogar, conectando generaciones a través de la magia de la
+          panadería artesanal. Nuestra misión es nutrir corazones y cuerpos con
+          productos horneados con pasión, generando momentos que perduran en el
+          tiempo y alimentan el alma de las familias.
+          <br /> <br />
+          Valores: La esencia de Bosque reside en el amor y la dedicación de
+          nuestra familia, que ha cuidado cada receta y cada proceso con cariño
+          durante tres generaciones. Honramos el legado de nuestro viejo horno a
+          leña, fusionando la autenticidad de antaño con la innovación de hoy
+          para ofrecer experiencias únicas y sabores inolvidables. No
+          comprometemos en lo que respecta al sabor y la frescura. Más que una
+          panadería, somos un punto de encuentro donde las personas se reúnen
+          para compartir, celebrar y crear recuerdos. Valoramos nuestra conexión
+          con la comunidad y la enriquecemos con cada pan que horneamos.
+          Buscamos inspirar no solo con nuestros productos, sino también con la
+          pasión que ponemos en todo lo que hacemos. Nuestra creatividad y
+          dedicación se reflejan en cada detalle.
+        </h6>
+      </div>
     </div>
   );
 }
