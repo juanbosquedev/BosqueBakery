265bed9436863057eb921ce76fee1a93f60aadcc
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-08-23 10:13:46 -0300

    card component

--- src/prodcuts/productCards.jsx
+++ src/prodcuts/productCards.jsx
@@ -1,9 +1,9 @@
+import products from "./branchJSON.JS";
 export default function ProductsCards() {
     return (
       <div id="productos">
-        <h1>
-   card
-        </h1>
+        <h6>{products[0].name}</h6>
+      <img src={products[0].Img} alt='producto'/>
       </div>
     );
   }
