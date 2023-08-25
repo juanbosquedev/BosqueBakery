b415d078eb928c5debe59125cd4cf994e7f4b08c
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-08-25 12:12:05 -0300

    card stuyles

--- src/prodcuts/productCards.jsx
+++ src/prodcuts/productCards.jsx
@@ -1,10 +1,22 @@
 import products from "./branchJSON.JS";
+import style from "./productCards.module.css";
 export default function ProductsCards() {
-    return (
-      <div id="productos">
-        <h6>{products[0].name}</h6>
-      <img src={products[0].Img} alt='producto'/>
-      </div>
-    );
-  }
-  
+  console.log({ products });
+  return (
+    <div id="productos" className={style.productCard_body}>
+      {products &&
+        products.map((product) => {
+          return (
+            <div key={product.name} className={style.productCard_img_body}>
+              <h6>{product.name}</h6>
+              <img
+                src={product.Img}
+                alt="producto"
+                className={style.productCard_img}
+              />
+            </div>
+          );
+        })}
+    </div>
+  );
+}
