2ae96761093160c0df144ad3d2f72b0df1e6efd7
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-08-25 11:08:04 -0300

    branches js

--- src/branches/branches.jsx
+++ src/branches/branches.jsx
@@ -5,17 +5,17 @@ import style from "./branches.module.css";
 function Branches() {
   return (
     <div id="sucursales">
-      {branches.map((branche) => (
-        <div className={style.branche} key={branche.name}>
-          <div className={style.branche_body}>
-            <main className={style.branche_main}>
-              {branche.name}
-              <br />
-              {branche.adress}
-            </main>
+      {branches &&
+        branches.map((branche) => (
+          <div className={style.branche} key={branche.name}>
+            <div className={style.branche_body}>
+              <main className={style.branche_main}>
+                {branche.name}
+                <br />
+                {branche.adress}
+              </main>
 
-            <div className={style.branche_map}>
-              {branche.adressImg && (
+              <div className={style.branche_map}>
                 <Link
                   to={branche.adressLink}
                   target="_blank"
@@ -26,14 +26,12 @@ function Branches() {
                     alt={branche.name}
                     className={style.branche_map_img}
                   />
-                  {/* <br /> */}
                   map
                 </Link>
-              )}
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 }
