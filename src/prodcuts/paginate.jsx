d379a3ddafa6e9a00f5f62007bce8e303bef8ca9
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-09-01 12:55:54 -0300

    paginate building

--- src/prodcuts/paginate.jsx
+++ src/prodcuts/paginate.jsx
@@ -1,10 +1,16 @@
-export default function Paginate (array){
-    let arr = array;
-    return(
-        <div>
-            {arr}
-        <h1>hola</h1>
-        </div>
-    )
+import { useState } from "react";
+import products from "./productsJSON.JS";
+export default function Paginate(array) {
+   const [count, setCount] = useState(0)
+  const total = array.length;
+  const pages = total / 2;
+  const perPage = array.slice
+  let arr = array;
+  return (
+    <div>
+      {arr}
+   
+      <h1>hola</h1>
+    </div>
+  );
 }
-
