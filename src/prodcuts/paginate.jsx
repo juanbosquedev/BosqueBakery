76c740436330fade8a2b471925b67bae874bbc93
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-09-03 11:38:39 -0300

    tyding code

--- src/prodcuts/paginate.jsx
+++ src/prodcuts/paginate.jsx
@@ -1,31 +1,32 @@
 import { useState } from "react";
 
 export default function Paginate(producto) {
-   const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   // const total = array.length;
   // const pages = total / 2;
   // const perPage = array.slice
   // // let arr = await array;
   const handleIncrement = () => {
-    if(count<= producto.producto.length-2){
-     setCount(count + 1);}
+    if (count <= producto.producto.length - 2) {
+      setCount(count + 1);
+    }
   };
 
   const handleDecrement = () => {
-    if(count> 0){
-   setCount(count - 1);}
-  };  return (
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+  return (
     <div>
       <div>
-       <h1>{producto.producto[count].name}</h1>
-      <img src={producto.producto[count].Img}></img>
-      <h1>{count}</h1>
-      <button onClick={handleDecrement}>-</button>
-      <button onClick={handleIncrement}>+</button><br/>
+        <h1>{producto.producto[count].name}</h1>
+        <img src={producto.producto[count].Img}></img>
 
+        <button onClick={handleDecrement}>-</button>
+        <button onClick={handleIncrement}>+</button>
+        <br />
       </div>
-
-      <h1>hola</h1>
     </div>
   );
 }
