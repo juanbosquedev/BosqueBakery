0d9b944a26d1f684bc10c8d184650d343b0a31a4
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-09-10 03:31:53 -0300

    useEffect products

--- src/prodcuts/paginate.jsx
+++ src/prodcuts/paginate.jsx
@@ -1,11 +1,10 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import style from "./paginate.module.css";
 
-export default function Paginate({producto}) {
+export default function Paginate({ producto }) {
   const [count, setCount] = useState(0);
-  const product = producto.map(el => el)
   const handleIncrement = () => {
-    if (count <= product.length - 2) {
+    if (count <= producto.length - 2) {
       setCount(count + 1);
     }
   };
@@ -14,21 +13,35 @@ export default function Paginate({producto}) {
     if (count > 0) {
       setCount(count - 1);
     }
+  
+  useEffect(() => {
+    handleDecrement();
+    handleIncrement();
+  }, [handleDecrement(), handleIncrement(), count]);
+  
   };
   return (
     <div className={style.paginate_body}>
-     
       <div className={style.paginate_content}>
-        <h1>{product[count].name}</h1>
+        <h1>{producto[count].name}</h1>
         <div className={style.image_container}>
-
-      
-      <button onClick={handleDecrement} className={style.paginate_content_btn_prev}> &lt; </button>
-        <img src={product[count].Img} className={style.paginate_image}></img>
-      <button onClick={handleIncrement} className={style.paginate_content_btn_next}> &gt; </button>
+          <button
+            onClick={handleDecrement}
+            className={style.paginate_content_btn_prev}
+          >
+            {" "}
+            &lt;{" "}
+          </button>
+          <img src={producto[count].Img} className={style.paginate_image}></img>
+          <button
+            onClick={handleIncrement}
+            className={style.paginate_content_btn_next}
+          >
+            {" "}
+            &gt;{" "}
+          </button>
+        </div>
       </div>
-      </div>
- 
     </div>
   );
 }
