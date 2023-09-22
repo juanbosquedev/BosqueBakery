3075bc72a17cd03aaf8c362f89f49e00f7cd5680
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-09-21 23:25:41 -0300

    interactive menu button responsive

--- src/nav/nav.jsx
+++ src/nav/nav.jsx
@@ -3,77 +3,70 @@ import style from "./nav.module.css";
 import logo from "../assets/logoBig.png";
 
 export default function Nav() {
-  const [showMenu, setShowMenu] = useState(true); // Estado para mostrar/ocultar el menú
-  const [showButton, setShowButton] = useState(false); // Estado para mostrar/ocultar el botón
+  const [showMenu, setShowMenu] = useState(true);
+  const [showButton, setShowButton] = useState(false);
+  const [small, setSmall] = useState(false);
 
-  // Función para manejar el cambio de tamaño de la ventana
   const handleResize = () => {
     if (window.innerWidth <= 600) {
       setShowMenu(false);
       setShowButton(true);
+      setSmall(true);
     } else {
       setShowMenu(true);
       setShowButton(false);
+      setSmall(false);
     }
   };
 
-  // Agregar un oyente de eventos para el cambio de tamaño de la ventana
   useEffect(() => {
-    handleResize(); // Llama a la función para configurar el estado inicial
+    handleResize();
     window.addEventListener("resize", handleResize);
     return () => {
       window.removeEventListener("resize", handleResize);
     };
-  }, []);
+  }, [small]);
 
   return (
     <div className={style.Nav} id="Nav">
       <img className={style.Nav_logo} src={logo} alt="logoBig" />
-      {/* Botón para mostrar/ocultar el menú */}
       {showButton && (
         <button
           className={style.Nav_button}
-          onClick={() => setShowMenu(!showMenu)}
+          onClick={() => {setShowButton(!showButton); setShowMenu(!showMenu)}}
         >
-          Mostrar Menú
+          Menú
         </button>
       )}
-      {/* Menú de navegación */}
       {showMenu && (
         <section className={style.Nav_main}>
-          <a href="#inicio" onClick={() => setShowMenu(!showMenu)}>
-            Inicio
-          </a>
-          <br />
-          <a href="#productos" onClick={() => setShowMenu(!showMenu)}>
+    <a href="#inicio" onClick={() => { small && setShowMenu(!showMenu); setShowButton(!showButton); }}>
+  Inicio
+</a>
+<br />
+
+          <a href="#productos" onClick={() => { small && setShowMenu(!showMenu); setShowButton(!showButton); }}>
             Productos
           </a>
           <br />
-          <a href="#nosotros" onClick={() => setShowMenu(!showMenu)}>
+          <a href="#nosotros" onClick={() => { small && setShowMenu(!showMenu); setShowButton(!showButton); }}>
             Nosotros
           </a>
           <br />
-          <a href="#sucursales" onClick={() => setShowMenu(!showMenu)}>
+          <a href="#sucursales" onClick={() => { small && setShowMenu(!showMenu); setShowButton(!showButton); }}>
             Sucursales
           </a>
           <br />
-          <a href="#contacto" onClick={() => setShowMenu(!showMenu)}>
+          <a href="#contacto" onClick={() => { small && setShowMenu(!showMenu); setShowButton(!showButton); }}>
             Contacto
           </a>
         </section>
       )}
-      {/* Menú desplegable */}
       {showMenu && (
         <div
           className={style.Nav_dropdown}
-          style={{ display: showButton ? "none" : "block" }}
-        >
-          {/* <a href="#inicio">Inicio</a>
-          <a href="#productos">Productos</a>
-          <a href="#nosotros">Nosotros</a>
-          <a href="#sucursales">Sucursales</a>
-          <a href="#contacto">Contacto</a> */}
-        </div>
+          style={{ display: showMenu ? "none" : "block" }}
+        ></div>
       )}
     </div>
   );
